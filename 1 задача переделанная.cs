3d6b7e5984db643f501248bef0c1198c35eb203d
PavelAnufriev <48158058+PavelAnufriev@users.noreply.github.com>
2019-05-11 02:21:58 +0500

    Add files via upload

---
+++ 1 задача переделанная.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static int Boats(double D, List<double> M)//1 500
+        {
+            if (D >= 15000 && M.Count >= 15000 && !(D > 0 )&& !(M.Count > 0))
+            {
+                return 0;
+
+            }
+            for (int i = 0; i < M.Count; i++)
+            {
+                if (M[i] > D)
+                {
+                    return 0;
+                }
+            }
+
+            int minIndex = 0, count = 0;
+            while (M.Count > 0)
+            {
+                double min = -1;
+                for (int i = 1; i < M.Count; i++)
+                {
+                    double d = D - M[0] - M[i];//Разница между грузоподъёмностью и суммарной массой пассажиров
+                    if (d >= 0 &&
+                        (d < min || min == -1))
+                    {
+                        min = d;
+                        minIndex = i;
+                    }
+                }
+                if (min == -1)
+                {
+                    M.Remove(M[0]);
+                    count++;
+                }
+                else
+                {
+                    M.Remove(M[minIndex]);
+                    M.Remove(M[0]);
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static string[] Read(string adress)
+        {
+            using (StreamReader streamReader = new StreamReader(adress, Encoding.Default))
+            {
+                return streamReader.ReadToEnd().
+                    Split(new Char[] { ' ', ',', '.', ':', ';', '?', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+
+        static void Write(string adress, int text)
+        {
+            using (StreamWriter sw = new StreamWriter(adress, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(text);
+            }
+        }
+        static void Main(string[] args)
+        {
+            IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+            string
+                input = @"INPUT.txt",
+                output = @"OUTPUT.txt";
+
+
+            try
+            {
+                string[] data = Read(input);
+
+                int n = int.Parse(data[0]);
+                double D = double.Parse(data[1], formatter);
+                List<double> M = new List<double>();
+                for (int i = 0; i < n; i++)
+                    M.Add(double.Parse(data[i + 2], formatter));
+                Write(output, Boats(D, M));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение");
+            }
+
+        }
+    }
+}
---
+++ 2 задача переделанная.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_task
+{
+    class Program
+    {
+
+
+        static double[,] Floyd(double[,] d)//2 300
+        {
+            int INF = 100000000, n = (int)Math.Sqrt(d.Length);
+            if(!(1<=n&&n<=100))
+                return null;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (Math.Abs(d[i, j]) <= 100)
+                        return null;
+                }
+            }
+            for (int k = 0; k < n; ++k)
+                for (int i = 0; i < n; ++i)
+                    for (int j = 0; j < n; ++j)
+                        if (d[i, k] < INF && d[k, j] < INF)
+                            if (d[i, j] > d[i, k] + d[k, j])
+                            {
+                                d[i, j] = d[i, k] + d[k, j];
+                            }
+            return d;
+        }
+
+        static string[] Read(string adress)
+        {
+            using (StreamReader streamReader = new StreamReader(adress, Encoding.Default))
+            {
+                return streamReader.ReadToEnd().
+                    Split(new Char[] { ' ', ',', '.', ':', ';', '?', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+
+        static void Write(string adress, double[,] M)
+        {
+            int n = (int)Math.Sqrt(M.Length);
+            using (StreamWriter sw = new StreamWriter(adress, false, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        sw.Write($"{M[i, j]} ");
+                    }
+                    sw.WriteLine();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+            string
+                input = @"INPUT.TXT",
+                output = @"OUTPUT.TXT";
+
+
+            try
+            {
+                string[] data = Read(input);
+                int n = int.Parse(data[0]),
+                    c = 1;
+                double[,] M = new double[n, n];
+
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        M[i, j] = double.Parse(data[c], formatter);
+                        c++;
+                    }
+
+                Write(output, Floyd(M));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Исключение");
+            }
+
+        }
+
+    }
+
+}
+
+
